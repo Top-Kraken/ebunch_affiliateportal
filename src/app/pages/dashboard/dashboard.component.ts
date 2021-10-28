b7bf3b4cbb6a34206c4ac33896d4f2f258f91920
Top-Kraken <topkyle521@gmail.com>
2021-10-29 00:26:55 +0530

    added style for mobile devices

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -31,15 +31,12 @@ export class DashboardComponent implements OnInit {
   dataSource = ELEMENT_DATA;
   config: SwiperOptions = {
     pagination: { el: '.swiper-pagination', clickable: true },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
+    navigation:true,
    
     spaceBetween: 30
   };
   config1: SwiperOptions = {
-    pagination: { el: '.swiper-pagination', clickable: true },
+    pagination: { el: '.swiper-pagination1', clickable: true },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
